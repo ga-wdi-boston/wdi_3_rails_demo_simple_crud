143b2771b017fef0fbd2881314562d29a5ef2ea6
Alex Grant <grantovich@users.noreply.github.com>
2014-05-07 11:09:46 -0400

    Add various colors to players

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140507140931) do
+ActiveRecord::Schema.define(version: 20140507144823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(version: 20140507140931) do
     t.boolean  "is_alive"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "hair_color"
+    t.text     "skin_color"
+    t.text     "eye_color"
   end
 
 end
