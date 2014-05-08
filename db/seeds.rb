c6e4a0292e186c184c12c6c0b5964caed9f438d1
Alex Grant <grantovich@users.noreply.github.com>
2014-05-08 09:48:43 -0400

    Create some basic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Player.delete_all
+
+Player.create(name: 'Alex', job: 'Knight', health: 100, magic: 20, is_alive: false)
+Player.create(name: 'David', job: 'Wizard', health: 50, magic: 200, is_alive: true)
+Player.create(name: 'Tom', job: 'Blacksmith', health: 200, magic: 20, is_alive: true)
+Player.create(name: 'Ethan', job: 'Wizard', health: 40, magic: 120, is_alive: true)
+Player.create(name: 'Lisa', job: 'Cleric', health: 70, magic: 160, is_alive: true)
